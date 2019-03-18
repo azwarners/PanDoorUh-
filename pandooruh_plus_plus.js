3d7a4750b0b007b9671f66635f33dd7151894175
Nick Warner <quicknick.26@gmail.com>
2019-03-17 20:12:43 -0700

    removing jquery

--- pandooruh_plus_plus.js
+++ pandooruh_plus_plus.js
@@ -13,19 +13,21 @@ var artistBlacklist = [
 ];
 
 var fetchTrackInfo = function() {
+    if(document.getElementsByClassName('Marquee__wrapper__content')[0]) {
+        var t = document.getElementsByClassName('Marquee__wrapper__content')[0].innerHTML;
+    }
+    else {
+        var t = document.getElementsByClassName('Marquee__wrapper__content__child')[0].innerHTML;
+    }
     return {
-        'title': $('.nowPlayingTopInfo__current__trackName').filter(":first").text(),
-        'artist': $('.nowPlayingTopInfo__current__artistName').filter(":first").text(),
-        'album': $('.nowPlayingTopInfo__current__albumName').filter(":first").text()
+        'title': t,
+        'artist': document.getElementsByClassName('nowPlayingTopInfo__current__artistName')[0].innerHTML,
+        'album': document.getElementsByClassName('nowPlayingTopInfo__current__albumName')[0].innerHTML
     };
 };
 
 var checkForNewTrack = setInterval(function () {
     var track = fetchTrackInfo();
-
-    if($('.MiniCoachmarkBingeSkipper__x')) {
-       $('.MiniCoachmarkBingeSkipper__x').click();
-    }
 
     if (title != track.title || artist != track.artist || album != track.album) {
     	title = track.title ;
@@ -54,7 +56,9 @@ var isBlacklisted = function(name) {
 function analyzeTrack() {
     var titleInfo = title + ( isLive(title) ? " is a live track." : " is a studio track." );
     var artistInfo = artist + (isBlacklisted(artist) ? " is blacklisted." : " is not blacklisted.");
-    console.log(titleInfo + " " + artistInfo);
+    var d = new Date();
+    var t = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    console.log(titleInfo + " " + artistInfo + " " + t);
     var thumbDownButton = document.getElementsByClassName("TunerControl ThumbDownButton Tuner__Control__Button Tuner__Control__ThumbDown__Button");
     if(isLive(title) || isBlacklisted(artist)) {
         thumbDownButton[0].click();
